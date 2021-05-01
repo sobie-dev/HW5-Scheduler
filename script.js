f95e796135ce2aaee0885611c20fa0a8f5412638
Stephen Obie <sobie31@outlook.com>
2021-05-01 17:34:16 -0400

    added js and html

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+var text = $('.textInput');
+var saveBtn = $('.saveBtn');
 
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
-
-// Get the current time and use to manipulate colors (past/present/future)
 var currentTime = moment().hours();
-console.log(currentTime, 'time')
+
 $('.time').each(function(){
     var HTMLTime = parseInt($(this).attr('id').split('-')[1])
-    console.log (HTMLTime, 'HTMLTime')
-
+    
     if (HTMLTime < currentTime) {
         $(this.children[1]).addClass('past')
     }
@@ -22,3 +21,17 @@ $('.time').each(function(){
         $(this.children[1]).addClass('future')
     }
 })
+saveBtn.on('click',function(event){
+    var userInput = $(this).siblings('.textInput').val()
+    var time = $(this).parent().attr('id')
+    localStorage.setItem(time,userInput);  
+})
+$('#hour-9 .textInput').val(localStorage.getItem('hour-9'))
+$('#hour-10 .textInput').val(localStorage.getItem('hour-10'))
+$('#hour-11 .textInput').val(localStorage.getItem('hour-11'))
+$('#hour-12 .textInput').val(localStorage.getItem('hour-12'))
+$('#hour-13 .textInput').val(localStorage.getItem('hour-13'))
+$('#hour-14 .textInput').val(localStorage.getItem('hour-14'))
+$('#hour-15 .textInput').val(localStorage.getItem('hour-15'))
+$('#hour-16 .textInput').val(localStorage.getItem('hour-16'))
+$('#hour-17 .textInput').val(localStorage.getItem('hour-17'))

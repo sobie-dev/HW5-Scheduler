1e3bad2f8e1c6c25967e6258e1e5bff08f738908
Stephen Obie <sobie31@outlook.com>
2021-04-30 20:40:24 -0400

    added JS and timeblock

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
+
+
+// Get the current time and use to manipulate colors (past/present/future)
+var currentTime = moment().hours();
+console.log(currentTime, 'time')
+$('.time').each(function(){
+    var HTMLTime = parseInt($(this).attr('id').split('-')[1])
+    console.log (HTMLTime, 'HTMLTime')
+
+    if (HTMLTime < currentTime) {
+        $(this.children[1]).addClass('past')
+    }
+    else if (HTMLTime === currentTime) {
+        $(this.children[1]).removeClass('past')
+        $(this.children[1]).addClass('present')
+    }
+    else {
+        $(this.children[1]).removeClass('past')
+        $(this.children[1]).removeClass('present')
+        $(this.children[1]).addClass('future')
+    }
+})
